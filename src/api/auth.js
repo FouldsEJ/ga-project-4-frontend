66a1a8e5ef629e17333d493f719cc655d84cbde4
Edward Foulds <ed2494@googlemail.com>
2022-05-19 10:16:46 -0400

    API endpoint changed to Heroku

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const baseUrl = `http://127.0.0.1:8000/`;
-// const baseUrl = `https://spikesquad.herokuapp.com/`;
+// const baseUrl = `http://127.0.0.1:8000/`;
+const baseUrl = `https://spikesquad.herokuapp.com/`;
 
 export const getAllUsers = async (username_search, user_id) => {
   console.log(username_search, user_id);
